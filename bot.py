2be66a4d9d5d73999023c2ad23d5ef9bbed1b2aa
Denob03 <denobili.luca@outlook.it>
2018-11-17 20:35:33 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,15 @@
 import discord
+from discord.ext.commands import Bot
 from discord.ext import commands
-TOKEN = 'MiPBQWwYYkNRGhpZtOwTJRC_uvt4C9AM'
-client = commands.Bot(command_prefix = '.')
-@client_event
+import asyncio
+import time
+ Client = discord.Client() #Initialise Client
+client = commands.Bot(command_prefix = "?") #Initialise client bot
+ @client.event
 async def on_ready():
-    print ('Bot is ready. ')
-client.run('TOKEN')
+    print("Bot is online and connected to Discord") #This will be called when the bot connects to the server
+ @client.event
+async def on_message(message):
+    if message.content == "cookie":
+        await client.send_message(message.channel, ":cookie:") #responds with Cookie emoji when someone says "cookie"
+client.run('MiPBQWwYYkNRGhpZtOwTJRC_uvt4C9AM') #Replace token with your bots token
